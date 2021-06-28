c76563e5af4a65338d4e42282457be5377c5aa34
Dmitry <dmitry.balduev@gmail.com>
2021-06-27 23:04:54 -0400

    added api routes

---
+++ routes/api.js
@@ -0,0 +1,47 @@
+const db = require("../models");
+const router = require("express").Router();
+
+router.get("/api/workouts", function (req, res) {
+    db.Workout.find({})
+        .then(function (workouts) {
+            res.json(workouts)
+        })
+        .catch(function (err) {
+            console.log(err)
+        })
+})
+
+router.post("/api/workouts", function (req, res) {
+    db.Workout.create({})
+        .then(function (workouts) {
+            res.json(workouts)
+        })
+        .catch(function (err) {
+            console.log(err)
+        })
+})
+
+router.put("/api/workouts/:id", function (req, res){
+    console.log(req.body)
+    db.Workout.where('_id', req.params.id).update({$push: {"exercises": req.body}})
+    .then(function(results){
+        res.json(results)
+    })
+    .catch(function (err) {
+        console.log(err)
+    })
+})
+
+router.get("/api/workouts/range", function(req, res){
+    db.Workout.find({})
+    .then(function (workouts) {
+        res.json(workouts)
+    })
+    .catch(function (err) {
+        console.log(err)
+    })
+})
+
+
+
+module.exports = router;
